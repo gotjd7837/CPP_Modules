3cf9625b1e64897b7803dff67df905c4a27dd5c2
Haeseong Kang <gotjd7837@gmail.com>
2024-01-17 18:56:39 +0900

    cpp 00 ex00

---
+++ CPP_Module_00/ex00/megaphone.cpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: haekang <haekang@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/17 14:24:25 by haekang           #+#    #+#             */
+/*   Updated: 2024/01/17 17:22:16 by haekang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    int i;
+    int j;
+
+    i = 1;
+    if (argc == 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    else
+    {
+        while (i < argc)
+        {
+            j = 0;
+            while (argv[i][j])
+            {
+                std::cout << (char)toupper(argv[i][j]);
+                j++;
+            }
+            i++;
+        }
+    }
+    std::cout << std::endl;
+    return (0);
+}
