b2bf132c86919cbe584f5c7497e60005b47b05f2
Haeseong Kang <gotjd7837@gmail.com>
2024-01-30 08:41:34 +0900

    c 00

--- CPP_Module_00/ex02/Account.cpp
+++ CPP_Module_00/ex02/Account.cpp
@@ -6,16 +6,44 @@
 /*   By: haekang <haekang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 17:37:08 by haekang           #+#    #+#             */
-/*   Updated: 2024/01/24 17:39:32 by haekang          ###   ########.fr       */
+/*   Updated: 2024/01/28 06:06:14 by haekang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "Account.hpp"
 
+int Account::_nbAccounts = 0;
+
+int Account::_totalAmount = 0;
+
+int Account::_totalNbDeposits = 0;
+
+int Account::_totalNbWithdrawals = 0;
+
+int Account::getNbAccounts(void)
+{
+    return (Account::_nbAccounts);
+}
+
+int Account::getTotalAmount(void)
+{
+    return (Account::_totalAmount);
+}
+
+int Account::getNbDeposits(void)
+{
+    return (Account::_totalNbDeposits);
+}
+
+int Account::getNbWithdrawals(void)
+{
+    return (Account::_totalNbWithdrawals);
+}
+
 Account::Account(int initial_deposit)
 {
-    this->_accountIndex = Account::_nbAccounts;
+    this->_accountIndex = Account::getNbAccounts();
     Account::_nbAccounts++;
     Account::_totalAmount += initial_deposit;
     this->_amount = initial_deposit;
@@ -23,4 +51,74 @@ Account::Account(int initial_deposit)
     this->_nbWithdrawals = 0;
     Account::_displayTimestamp();
     std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";created" << std::endl;
+}
+
+Account::~Account(void)
+{
+    Account::_displayTimestamp();
+    std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";closed" << std::endl;
+}
+
+void Account::makeDeposit(int deposit)
+{
+    Account::_displayTimestamp();
+    std::cout << "index:" << this->_accountIndex << ";p_amount:" << this->_amount << ";deposit:" << deposit;
+    this->_amount += deposit;
+    Account::_totalAmount += deposit;
+    this->_nbDeposits++;
+    Account::_totalNbDeposits++;
+    std::cout << ";amount:" << this->_amount << ";nb_deposits:" << this->_nbDeposits << std::endl;
+}
+
+bool Account::makeWithdrawal(int withdrawal)
+{
+    Account::_displayTimestamp();
+    std::cout << "index:" << this->_accountIndex << ";p_amount:" << this->_amount << ";withdrawal:";
+    if (withdrawal > this->_amount)
+    {
+        std::cout << "refused" << std::endl;
+        return (false);
+    }
+    this->_amount -= withdrawal;
+    Account::_totalAmount -= withdrawal;
+    this->_nbWithdrawals++;
+    Account::_totalNbWithdrawals++;
+    std::cout << withdrawal << ";amount:" << this->_amount << ";nb_withdrawals:" << this->_nbWithdrawals << std::endl;
+    return (true);
+}
+
+// int Account::checkAmount(void) const
+// {
+//     return (this->_amount);
+// }
+
+void Account::displayStatus(void) const
+{
+    Account::_displayTimestamp();
+    std::cout << "index:" << this->_accountIndex << ";amount:";
+    std::cout << this->_amount << ";deposits:" << this->_nbDeposits << ";withdrawals:";
+    std::cout << this->_nbWithdrawals << std::endl;
+}
+
+void Account::_displayTimestamp(void)
+{
+    std::time_t now = std::time(0);
+    std::tm *ltm = std::localtime(&now);
+    std::cout << "[" << 1900 + ltm->tm_year;
+    if (1 + ltm->tm_mon < 10)
+        std::cout << "0";
+    std::cout <<  1 + ltm->tm_mon;
+    std::cout << ltm->tm_mday << "_";
+    std::cout << ltm->tm_hour;
+    std::cout << ltm->tm_min;
+    std::cout << ltm->tm_sec << "] ";
+}
+
+void Account::displayAccountsInfos(void)
+{
+    Account::_displayTimestamp();
+    std::cout << "accounts:" << Account::getNbAccounts();
+    std::cout << ";total:" << Account::getTotalAmount();
+    std::cout << ";deposits:" << Account::getNbDeposits();
+    std::cout << ";withdrawals:" << Account::getNbWithdrawals() << std::endl;
 }
